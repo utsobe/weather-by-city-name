c34ed31500e1d1b6493639f0ea9242bb164a94ea
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-03-02 20:58:27 +0600

    weather details added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const API_KEY = `4b0e740606a6e3d318b40d77b6383f62`;
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     console.log(url);
 
     fetch(url)
@@ -9,6 +9,16 @@ const searchTemperature = () => {
         .then(data => displayTemperature(data))
 };
 
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+};
+
 const displayTemperature = temperature => {
-    console.log(temperature);
-}
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    // set weather icon
+    const url = ` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+};
